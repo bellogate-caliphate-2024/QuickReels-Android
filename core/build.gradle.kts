73a29c776da158d10760a9464ee968dee4caa08c
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2024-09-15 20:53:14 +0100

    update

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,46 +1,53 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.serialization)
+	alias(libs.plugins.androidLibrary)
+	alias(libs.plugins.jetbrainsKotlinAndroid)
+	alias(libs.plugins.serialization)
+	id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.bellogatecaliphate.core"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+	namespace = "com.bellogatecaliphate.core"
+	compileSdk = 34
+	
+	defaultConfig {
+		minSdk = 24
+		
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
+	
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt") ,
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    // serialization
-    implementation(libs.kotlinx.serialization.json)
+	
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.appcompat)
+	implementation(libs.material)
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
+	// serialization
+	implementation(libs.kotlinx.serialization.json)
+	
+	// Room
+	implementation(libs.androidx.room.runtime)
+	annotationProcessor(libs.androidx.room.compiler)
+	kapt(libs.androidx.room.compiler)
+	implementation(libs.androidx.room.ktx)
 }
