6df15dcc42a8267ccefebc67ae36628e96759a4f
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2024-09-15 12:46:34 +0100

    update

--- feature/create_post/src/main/java/com/bellogatecaliphate/create_post/ui/preview_post/PreviewPost.kt
+++ feature/create_post/src/main/java/com/bellogatecaliphate/create_post/ui/preview_post/PreviewPost.kt
@@ -1,11 +1,9 @@
 package com.bellogatecaliphate.create_post.ui.preview_post
 
 import android.net.Uri
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,11 +14,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
@@ -29,17 +25,15 @@ import androidx.media3.ui.PlayerView
 
 @Composable
 fun PreviewPost(videoPath : String = "" , onSendButtonClicked : () -> Unit = {}) {
-	Box(Modifier.fillMaxSize()) {
-		VideoPreview(videoPath)
-		Column(modifier = Modifier.align(Alignment.BottomCenter)) {
-			VideoDescriptionSection()
-			SendButton(onSendButtonClicked)
-		}
+	Column(Modifier.fillMaxSize()) {
+		VideoPreview(Modifier.weight(1f) , videoPath)
+		VideoDescriptionSection()
+		SendButton(onSendButtonClicked)
 	}
 }
 
 @Composable
-private fun VideoPreview(videoPath : String) {
+private fun VideoPreview(modifier : Modifier , videoPath : String) {
 	val context = LocalContext.current
 	val uri = Uri.parse(videoPath)
 	val exoPlayer = remember {
@@ -52,7 +46,7 @@ private fun VideoPreview(videoPath : String) {
 	
 	DisposableEffect(
 		AndroidView(
-			modifier = Modifier.size(100.dp , 100.dp) ,
+			modifier = modifier ,
 			factory = {
 				PlayerView(context).apply { player = exoPlayer }
 			})
