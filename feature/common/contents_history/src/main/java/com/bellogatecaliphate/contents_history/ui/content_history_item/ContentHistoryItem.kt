2c754611a7e12eb2f9ba5bdf403d76f8e3f2e68c
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2024-12-07 11:20:57 +0100

    update

--- feature/common/contents_history/src/main/java/com/bellogatecaliphate/contents_history/ui/content_history_item/ContentHistoryItem.kt
+++
@@ -1,9 +0,0 @@
-package com.bellogatecaliphate.contents_history.ui.content_history_item
-
-import androidx.compose.runtime.Composable
-import com.bellogatecaliphate.core.model.dto.Content
-
-@Composable
-internal fun ContentHistoryItem(content: Content) {
-
-}
