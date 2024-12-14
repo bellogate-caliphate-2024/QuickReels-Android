d6168bea145555d73aa09b8bcad7b80b9d310d71
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2024-12-14 23:01:52 +0100

    update

---
+++ feature/timeline/src/main/java/com/bellogatecaliphate/timeline/ui/TimelineScreen.kt
@@ -0,0 +1,28 @@
+package com.bellogatecaliphate.timeline.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.bellogatecaliphate.core.ui.ProgressBar
+import com.bellogatecaliphate.timeline.model.UiState
+import com.bellogatecaliphate.timeline.ui.timeline.content.Contents
+
+@Composable
+fun TimeLineScreen(viewModel: TimeLineScreenViewModel = hiltViewModel()) {
+	
+	val contents = viewModel.getContents().collectAsLazyPagingItems()
+	viewModel.setContents(contents)
+	
+	val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+	TimeLineScreen(uiState.value)
+}
+
+@Composable
+private fun TimeLineScreen(uiState: UiState) {
+	Column {
+		ProgressBar(uiState.loading)
+		Contents(uiState.listOfContents)
+	}
+}
---
+++ feature/timeline/src/main/java/com/bellogatecaliphate/timeline/ui/content/ContentsUi.kt
@@ -0,0 +1,46 @@
+package com.bellogatecaliphate.timeline.ui.content
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import com.bellogatecaliphate.core.model.dto.Content
+import kotlinx.coroutines.launch
+
+@Composable
+fun Contents(list: LazyPagingItems<Content>?) {
+	if (list == null) return
+	val listState = rememberLazyListState()
+	val coroutineScope = rememberCoroutineScope()
+	
+	LaunchedEffect(listState) {
+		snapshotFlow { listState.isScrollInProgress }
+			.collect { isScrolling ->
+				if (! isScrolling) {
+					// Snaps to the closest item when scrolling stops
+					val itemIndex = listState.firstVisibleItemIndex
+					val itemOffset = listState.firstVisibleItemScrollOffset
+					
+					coroutineScope.launch {
+						// Snap to the next item if scrolled more than halfway
+						if (itemOffset > 0 && itemOffset > listState.layoutInfo.viewportSize.height / 2) {
+							listState.animateScrollToItem(itemIndex + 1)
+						} else {
+							listState.animateScrollToItem(itemIndex)
+						}
+					}
+				}
+			}
+	}
+	LazyColumn(state = listState, modifier = Modifier.fillMaxSize()) {
+		items(list.itemCount) { index ->
+			val content = list[index]
+			content?.let { ContentUi(it, Modifier.fillParentMaxSize()) }
+		}
+	}
+}
