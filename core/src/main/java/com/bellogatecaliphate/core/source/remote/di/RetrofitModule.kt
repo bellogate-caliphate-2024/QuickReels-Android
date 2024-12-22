1eb41e5088d1e1ab2c8269d4df1085dfcbc75b5b
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2024-12-23 00:28:06 +0100

    update

--- core/src/main/java/com/bellogatecaliphate/core/source/remote/di/RetrofitModule.kt
+++ core/src/main/java/com/bellogatecaliphate/core/source/remote/di/RetrofitModule.kt
@@ -1,9 +1,11 @@
 package com.bellogatecaliphate.core.source.remote.di
 
-import com.bellogatecaliphate.core.source.remote.RetrofitBuilder
+import android.content.Context
+import com.bellogatecaliphate.core.source.remote.RetrofitClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 
@@ -12,8 +14,7 @@ import retrofit2.Retrofit
 object RetrofitModule {
 	
 	@Provides
-	fun provideRetrofit(): Retrofit {
-		return RetrofitBuilder.getRetrofit()
+	fun provideRetrofit(@ActivityContext context: Context): Retrofit {
+		return RetrofitClient.getRetrofit(context)
 	}
-	
 }
