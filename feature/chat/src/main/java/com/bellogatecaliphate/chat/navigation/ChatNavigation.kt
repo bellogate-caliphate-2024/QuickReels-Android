1bb35b8d20ffb25dc39d7ec77de92808888cedf8
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-01-04 11:01:51 +0100

    update

--- feature/chat/src/main/java/com/bellogatecaliphate/chat/navigation/ChatNavigation.kt
+++ feature/chat/src/main/java/com/bellogatecaliphate/chat/navigation/ChatNavigation.kt
@@ -4,13 +4,19 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import androidx.navigation.toRoute
 import com.bellogatecaliphate.chat.ui.SelectUserScreen
+import com.bellogatecaliphate.chat.ui.chat_room.ChatRoom
 import com.bellogatecaliphate.core.model.routes.chat.ChatNavGraphRoute
 
 fun NavGraphBuilder.chatNavGraph(navController: NavHostController) {
 	navigation<ChatNavGraphRoute>(startDestination = ChatNavGraphRoute.SelectUser::class) {
 		composable<ChatNavGraphRoute.SelectUser> {
 			SelectUserScreen { navController.navigate(ChatNavGraphRoute.Chat(it.email)) }
+		}
+		composable<ChatNavGraphRoute.Chat> { backStackEntry ->
+			val chatUser = backStackEntry.toRoute<ChatNavGraphRoute.Chat>()
+			ChatRoom(chatUser.chatUserId)
 		}
 	}
 }
--- feature/chat/src/main/java/com/bellogatecaliphate/chat/ui/chat_room/ChatRoom.kt
+++ feature/chat/src/main/java/com/bellogatecaliphate/chat/ui/chat_room/ChatRoom.kt
@@ -1,2 +1,10 @@
 package com.bellogatecaliphate.chat.ui.chat_room
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatRoom(chatUserId: String) {
+	Text(text = "Hello you are chatting with $chatUserId")
+}
+
