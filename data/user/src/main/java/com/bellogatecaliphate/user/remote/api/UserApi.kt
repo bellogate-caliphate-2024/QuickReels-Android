85ccdc084a539e1a1747027dd80e172488589d1c
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-01-05 18:30:03 +0100

    working

--- data/user/src/main/java/com/bellogatecaliphate/user/remote/api/UserApi.kt
+++ data/user/src/main/java/com/bellogatecaliphate/user/remote/api/UserApi.kt
@@ -8,7 +8,10 @@ import retrofit2.http.Query
 interface UserApi {
 	
 	@GET("get_users")
-	suspend fun getUsers(page: Int, numberOfContentPerPage: Int): UsersResponse?
+	suspend fun getUsers(
+		@Query("page") page: Int,
+		@Query("numberOfContentPerPage") numberOfContentPerPage: Int
+	): UsersResponse?
 	
 	@GET("get_user")
 	suspend fun getUser(userEmail: String): UserResponse?
