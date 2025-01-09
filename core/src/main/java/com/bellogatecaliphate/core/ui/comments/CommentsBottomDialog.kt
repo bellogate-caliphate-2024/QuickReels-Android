c6645d20964180fe0da7296671ae8b41c2268020
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-01-09 09:36:34 +0100

    working

--- core/src/main/java/com/bellogatecaliphate/core/ui/comments/CommentsBottomDialog.kt
+++ core/src/main/java/com/bellogatecaliphate/core/ui/comments/CommentsBottomDialog.kt
@@ -1,10 +1,5 @@
 package com.bellogatecaliphate.core.ui.comments
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -13,12 +8,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.bellogatecaliphate.core.model.dto.Comment
-import com.bellogatecaliphate.core.ui.comments.comments_list.CommentsList
-import com.bellogatecaliphate.core.ui.comments.no_comment.NoComment
-import com.bellogatecaliphate.core.util.PLACEHOLDER_16DP
+import com.bellogatecaliphate.core.ui.comments.dialog_content.Content
 
 /**
  * @param visible is true if the CommentsBottomDialog should be visible.
@@ -67,43 +58,5 @@ internal fun CommentsBottomDialog(
 			onSaveReply,
 			onLoadReplies
 		)
-	}
-}
-
-@Composable
-private fun Content(
-	isLoadingInitialComments: Boolean,
-	noCommentsFound: Boolean,
-	listOfComments: List<Comment>,
-	isLoadingReplies: Boolean = false,
-	listOfReplies: List<Comment> = emptyList(),
-	repliesPageNumber: Int? = null,
-	canLoadMoreReplies: Boolean = false,
-	onSaveReply: ((originalCommentId: String, reply: String) -> Unit)? = null,
-	onLoadReplies: (pageNumber: Int) -> Unit = {},
-) {
-	Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-		when {
-			isLoadingInitialComments    -> {
-				CircularProgressIndicator()
-			}
-			
-			noCommentsFound             -> {
-				NoComment()
-			}
-			
-			listOfComments.isNotEmpty() -> {
-				CommentsList(
-					listOfComments,
-					isLoadingReplies,
-					listOfReplies,
-					repliesPageNumber,
-					canLoadMoreReplies,
-					onSaveReply,
-					onLoadReplies
-				)
-			}
-		}
-		Spacer(modifier = Modifier.height(PLACEHOLDER_16DP))
 	}
 }
---
+++ core/src/main/java/com/bellogatecaliphate/core/ui/comments/dialog_content/Content.kt
@@ -0,0 +1,52 @@
+package com.bellogatecaliphate.core.ui.comments.dialog_content
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.bellogatecaliphate.core.model.dto.Comment
+import com.bellogatecaliphate.core.ui.comments.comments_list.CommentsList
+import com.bellogatecaliphate.core.ui.comments.no_comment.NoComment
+import com.bellogatecaliphate.core.util.PLACEHOLDER_16DP
+
+@Composable
+internal fun Content(
+	isLoadingInitialComments: Boolean,
+	noCommentsFound: Boolean,
+	listOfComments: List<Comment>,
+	isLoadingReplies: Boolean = false,
+	listOfReplies: List<Comment> = emptyList(),
+	repliesPageNumber: Int? = null,
+	canLoadMoreReplies: Boolean = false,
+	onSaveReply: ((originalCommentId: String, reply: String) -> Unit)? = null,
+	onLoadReplies: (pageNumber: Int) -> Unit = {},
+) {
+	Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+		when {
+			isLoadingInitialComments    -> {
+				CircularProgressIndicator()
+			}
+			
+			noCommentsFound             -> {
+				NoComment()
+			}
+			
+			listOfComments.isNotEmpty() -> {
+				CommentsList(
+					listOfComments,
+					isLoadingReplies,
+					listOfReplies,
+					repliesPageNumber,
+					canLoadMoreReplies,
+					onSaveReply,
+					onLoadReplies
+				)
+			}
+		}
+		Spacer(modifier = Modifier.height(PLACEHOLDER_16DP))
+	}
+}
