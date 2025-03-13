fd7d75f8fde0fd7face25890603a445eacb2e0ca
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-03-13 01:16:09 +0100

    fixed slow loading issue

--- feature/ads/nativeads/src/main/java/com/bellogatecaliphate/nativeads/QuickReelsNativeAd.kt
+++ feature/ads/nativeads/src/main/java/com/bellogatecaliphate/nativeads/QuickReelsNativeAd.kt
@@ -1,19 +1,33 @@
 package com.bellogatecaliphate.nativeads
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidViewBinding
 import com.bellogatecaliphate.nativeads.nativeadstemplates.databinding.QuickReelsNativeAdViewBinding
+import com.google.android.gms.ads.nativead.NativeAd
 
 @Composable
 fun QuickReelsNativeAd(modifier: Modifier, adLoader: QuickReelsNativeAdLoader) {
+	var isAdLoadedSuccessfully by remember { mutableStateOf(false) }
+	var cachedLoadedAd: NativeAd? by remember { mutableStateOf(null) }
+	
 	AndroidViewBinding(QuickReelsNativeAdViewBinding::inflate, modifier) {
-		adLoader.loadAd(
-			{ nativeAd ->
-				nativeAdTemplate.setNativeAd(nativeAd)
-			}, {
-				// do nothing on error
-			}
-		)
+		if (isAdLoadedSuccessfully && cachedLoadedAd != null) {
+			nativeAdTemplate.setNativeAd(cachedLoadedAd)
+		} else {
+			adLoader.loadAd(
+				{ nativeAd ->
+					isAdLoadedSuccessfully = true
+					cachedLoadedAd = nativeAd
+					nativeAdTemplate.setNativeAd(nativeAd)
+				}, {
+					// do nothing on error
+				}
+			)
+		}
 	}
 }
