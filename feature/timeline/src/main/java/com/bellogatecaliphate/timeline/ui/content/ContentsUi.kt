23e4304092501926018655106833453d1b7fdcde
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-03-15 11:24:11 +0100

    small code improvement

--- feature/timeline/src/main/java/com/bellogatecaliphate/timeline/ui/content/ContentsUi.kt
+++ feature/timeline/src/main/java/com/bellogatecaliphate/timeline/ui/content/ContentsUi.kt
@@ -41,13 +41,13 @@ internal fun Contents(
 		if (currentIndex == previousIndex.intValue && currentOffset > previousOffset.intValue) {
 			// The user is scrolling down the list to the bottom:
 			val nextItemIndex = listState.firstVisibleItemIndex + 1
-			listState.animateScrollToItem(nextItemIndex)
+			listState.scrollToItem(nextItemIndex)
 			
 		} else if (currentIndex == previousIndex.intValue && currentOffset < previousOffset.intValue) {
 			// The user is scrolling up the list to the top:
 			val previousItemIndex = listState.firstVisibleItemIndex
 			if (previousItemIndex >= 0) {
-				listState.animateScrollToItem(previousItemIndex)
+				listState.scrollToItem(previousItemIndex)
 			}
 		}
 		
