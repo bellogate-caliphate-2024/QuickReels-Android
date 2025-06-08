d0cf9e95d030fcfe75aa06bda355fb10ca9cd226
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-06-08 04:07:24 +0100

    working

--- feature/create_post/src/main/java/com/bellogatecaliphate/create_post/util/video_trimer/utils/TrimmerUtils.java
+++ feature/create_post/src/main/java/com/bellogatecaliphate/create_post/util/video_trimer/utils/TrimmerUtils.java
@@ -86,7 +86,7 @@ public class TrimmerUtils {
     public static String getFileExtension(Context context, Uri uri) {
         try {
             String extension;
-            if (uri.getScheme()!=null && uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            if (uri.getScheme() != null && uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
                 final MimeTypeMap mime = MimeTypeMap.getSingleton();
                 extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uri));
             } else
@@ -116,7 +116,7 @@ public class TrimmerUtils {
     public static int getFrameRate(Activity context, Uri videoPath) {
         try {
             MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            retriever.setDataSource(context,videoPath);
+            retriever.setDataSource(context, videoPath);
             int frameRate = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_FRAME_COUNT));
             retriever.release();
             return frameRate;
@@ -143,11 +143,11 @@ public class TrimmerUtils {
     public static int[] getVideoWidthHeight(Activity context, Uri videoPath) {
         try {
             MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            retriever.setDataSource(context,videoPath);
+            retriever.setDataSource(context, videoPath);
             int width = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
             int height = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
             retriever.release();
-            return new int[]{width,height};
+            return new int[]{width, height};
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -157,7 +157,7 @@ public class TrimmerUtils {
     public static int getVideoRotation(Activity context, Uri videoPath) {
         try {
             MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            retriever.setDataSource(context,videoPath);
+            retriever.setDataSource(context, videoPath);
             int rotation = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION));
             retriever.release();
             return rotation;
@@ -190,29 +190,33 @@ public class TrimmerUtils {
         return formattedTime;
     }
 
-    public static String getLimitedTimeFormatted(long secs){
-            long hours = secs / 3600;
-            long secondsLeft = secs - hours * 3600;
-            long minutes = secondsLeft / 60;
-            long seconds = secondsLeft - minutes * 60;
-            String time;
-            if (hours!=0){
-                time=hours+" Hrs "+(minutes!=0 ? minutes+" Mins " : "")+
-                        (seconds!=0 ? seconds+" Secs " : "");
-            }else if (minutes!=0)
-                time=minutes+" Mins "+(seconds!=0 ? seconds+" Secs ":"");
-            else
-                time=seconds+" Secs ";
-            LogMessage.v(time);
-            return time;
+    public static String getLimitedTimeFormatted(long secs) {
+        long hours = secs / 3600;
+        long secondsLeft = secs - hours * 3600;
+        long minutes = secondsLeft / 60;
+        long seconds = secondsLeft - minutes * 60;
+        String time;
+        if (hours != 0) {
+            time = hours + " Hours " + (minutes != 0 ? minutes + " Minutes " : "") +
+                    (seconds != 0 ? seconds + " Seconds " : "");
+        } else if (minutes != 0) {
+            if (minutes == 1) {
+                time = minutes + " Minute " + (seconds != 0 ? seconds + " Seconds " : "");
+            } else {
+                time = minutes + " Minutes " + (seconds != 0 ? seconds + " Seconds " : "");
+            }
+        } else
+            time = seconds + " Seconds ";
+        LogMessage.v(time);
+        return time;
     }
 
     public static String clearNull(String value) {
         return value == null ? "" : value.trim();
     }
 
-    public static boolean hasSpecialChar(String value){
-        value=clearNull(value);
+    public static boolean hasSpecialChar(String value) {
+        value = clearNull(value);
         Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(value);
         return m.find();
