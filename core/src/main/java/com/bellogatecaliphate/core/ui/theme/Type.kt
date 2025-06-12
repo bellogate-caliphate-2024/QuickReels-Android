2a7b23bd45d3e1e9bd5d2f53f8a2b942e7c65663
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-06-12 02:27:58 +0100

    working

--- core/src/main/java/com/bellogatecaliphate/core/ui/theme/Type.kt
+++ core/src/main/java/com/bellogatecaliphate/core/ui/theme/Type.kt
@@ -2,33 +2,30 @@ package com.bellogatecaliphate.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.bellogatecaliphate.core.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+	// bodyLarge: is typically the android default for main body text ie all Text composable on the app.
+	// So you don't need to manually add it to your text styles. They automatically refer to it.
+	// The bodyLarge style is often used as the default fallback for regular text.
+	// Note: Text composables inside a Button does not use this style. They use labelLarge by default.
 	bodyLarge = TextStyle(
-		fontFamily = FontFamily.Default,
+		fontFamily = FontFamily(Font(R.font.lato_regular)),
+		fontWeight = FontWeight.Normal,
+		fontSize = 16.sp,
+		lineHeight = 24.sp,
+		letterSpacing = 0.5.sp
+	),
+	// labelLarge: is typically the android default for Text composables inside Buttons.
+	labelLarge = TextStyle(
+		fontFamily = FontFamily(Font(R.font.lato_regular)),
 		fontWeight = FontWeight.Normal,
 		fontSize = 16.sp,
 		lineHeight = 24.sp,
 		letterSpacing = 0.5.sp
 	)
-	/* Other default text styles to override
-	titleLarge = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 22.sp,
-		lineHeight = 28.sp,
-		letterSpacing = 0.sp
-	),
-	labelSmall = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Medium,
-		fontSize = 11.sp,
-		lineHeight = 16.sp,
-		letterSpacing = 0.5.sp
-	)
-	*/
 )
