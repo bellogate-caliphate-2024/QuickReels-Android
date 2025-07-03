96a2f4a7a7bd108ea13e376be305ce0fa7bacdd2
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-07-04 00:55:11 +0100

    account screen ui update

--- feature/account/src/main/java/com/bellogatecaliphate/account/ui/screens/profile_detail/composables/UserAccountInformationSection.kt
+++ feature/account/src/main/java/com/bellogatecaliphate/account/ui/screens/profile_detail/composables/UserAccountInformationSection.kt
@@ -1,5 +1,8 @@
 package com.bellogatecaliphate.account.ui.screens.profile_detail.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,6 +41,7 @@ internal fun UserAccountInformationSection(
 		modifier = Modifier.fillMaxSize(),
 		horizontalAlignment = Alignment.CenterHorizontally
 	) {
+		var newAccountName: String? by remember { mutableStateOf(null) }
 		Column {
 			Text(
 				modifier = Modifier
@@ -47,8 +55,10 @@ internal fun UserAccountInformationSection(
 				modifier = Modifier
 					.fillMaxWidth()
 					.padding(PLACEHOLDER_16DP),
-				value = user?.accountName ?: "",
-				onValueChange = {},
+				value = newAccountName ?: user?.accountName ?: "",
+				onValueChange = {
+					newAccountName = it
+				},
 				label = { Text("Account Name") },
 				shape = RoundedCornerShape(8.dp),
 				colors = TextFieldDefaults.colors(
@@ -56,16 +66,8 @@ internal fun UserAccountInformationSection(
 					unfocusedIndicatorColor = Color.Transparent
 				)
 			)
-			Text(
-				modifier = Modifier
-					.fillMaxWidth()
-					.padding(horizontal = PLACEHOLDER_16DP)
-					.clickable { },
-				text = stringResource(R.string.save),
-				softWrap = true,
-				color = colorResource(com.bellogatecaliphate.core.R.color.quickreels_purple),
-				textAlign = TextAlign.Center,
-			)
+			val showSaveButton = newAccountName != null && newAccountName != user?.accountName
+			SaveButton(show = showSaveButton, onClick = {})
 		}
 		Text(
 			fontWeight = FontWeight.Bold,
@@ -76,6 +78,26 @@ internal fun UserAccountInformationSection(
 			text = stringResource(R.string.logout),
 			softWrap = true,
 			color = Color.Red,
+			textAlign = TextAlign.Center,
+		)
+	}
+}
+
+@Composable
+private fun SaveButton(show: Boolean, onClick: () -> Unit) {
+	AnimatedVisibility(
+		show,
+		enter = fadeIn(),
+		exit = fadeOut()
+	) {
+		Text(
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(horizontal = PLACEHOLDER_16DP)
+				.clickable { onClick() },
+			text = stringResource(R.string.save),
+			softWrap = true,
+			color = colorResource(com.bellogatecaliphate.core.R.color.quickreels_purple),
 			textAlign = TextAlign.Center,
 		)
 	}
