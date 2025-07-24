4523f79326ecc849b175f790b8481dfad60828be
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-07-25 00:06:29 +0100

    update

--- data/contents/src/main/java/com/bellogatecaliphate/contents/remote/RemoteSource.kt
+++ data/contents/src/main/java/com/bellogatecaliphate/contents/remote/RemoteSource.kt
@@ -114,8 +114,110 @@ internal class RemoteSource @Inject constructor(
 		
 		)
 	
+	val listOfContentHistory = listOf(
+		ContentResponse(
+			"119992299222",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/The%20Matrix%20-%20Neo%20Meets%20The%20Oracle.mp4?alt=media&token=d80f8dd3-2133-4631-be42-380bd9840d10",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/1.jpeg?alt=media&token=03fa3168-a47c-4f2a-b169-76505fbd364f",
+			"This is a test caption about a movie written by peter jackson. The movie is so good. It tells the tale of a sky ranger named Neo and the plan to take over the kingdom. This movie is exciting and very faced paced. You will be so thrilled with joy. I rate it a five stars.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"11",
+			"jeffemuveyan@gmail.com",
+			"Monday Manyy",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/p1.jpg?alt=media&token=630134db-35cb-41ea-9698-fa0cd25b80c0",
+			false,
+			false
+		),
+		ContentResponse(
+			"00000000",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/The%20Northman%20(2022)%20-%20Berserker%20Ritual%20Scene.mp4?alt=media&token=931fe661-ba3a-48ef-94d1-ae0b2ffae9ba",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/3.jpg?alt=media&token=a20702c8-b183-4d8f-8b27-f9da833b1319",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am",
+			"100",
+			"70",
+			"0",
+			"",
+			"Tuesday Tuee",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/p2.jpg?alt=media&token=518d902-2efc-4739-8b96-00be0fff78f",
+			false,
+			false
+		),
+		ContentResponse(
+			"1120022",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/avatar_official_trailer_hd_h264_60552.mp4?alt=media&token=6da2c5fd-1bef-4328-ad6f-ec9fe40d6513",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/3.jpg?alt=media&token=a20702c8-b183-4d8f-8b27-f9da833b1319",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"0",
+			"",
+			"Wednesday Wendy",
+			"", false,
+			false
+		),
+		ContentResponse(
+			"898989898",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/lord_of_the_rings_forbidden_edition_part_3_h264_60712.mp4?alt=media&token=ad7f0f2-851b-4fd2-b997-0cfb670fdf1d",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/3.jpg?alt=media&token=a20702c8-b183-4d8f-8b27-f9da833b1319",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"0",
+			"",
+			"Thursday Thur",
+			"", false,
+			false
+		),
+		ContentResponse(
+			"898989898",
+			"",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/1.jpeg?alt=media&token=03fa3168-a47c-4f2a-b169-76505fbd364f",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"0",
+			"",
+			"Friday Fry",
+			"", false,
+			false
+		),
+		ContentResponse(
+			"898989898",
+			"",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/1.jpeg?alt=media&token=03fa3168-a47c-4f2a-b169-76505fbd364f",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"0",
+			"",
+			"Saturday Saturn",
+			"", false,
+			false
+		),
+		ContentResponse(
+			"898989898",
+			"",
+			"https://firebasestorage.googleapis.com/v0/b/memo-24031.appspot.com/o/3.jpg?alt=media&token=a20702c8-b183-4d8f-8b27-f9da833b1319",
+			"This is a test caption about a movie written by peter jackson. The movie is so good.",
+			"05-05-200 - 10:30 am", "100",
+			"70",
+			"0",
+			"",
+			"Sunday Sunny",
+			"", false,
+			false
+		),
+		
+		)
+	
 	val contentsListResponse = ContentsListResponse(
 		1, null, listOfContent, true
+	)
+	
+	val contentsHistoryListResponse = ContentsListResponse(
+		1, null, listOfContentHistory, true
 	)
 	
 	override suspend fun getContentsList(
@@ -132,7 +234,7 @@ internal class RemoteSource @Inject constructor(
 		numberOfContentPerPage: Int
 	): ContentsListResponse {
 		delay(3_000)
-		return contentsListResponse
+		return contentsHistoryListResponse
 	}
 	
 	override suspend fun getContent(contentId: String): ContentResponse =
--- feature/account/src/main/java/com/bellogatecaliphate/account/ui/composables/ContentHistoryGridList.kt
+++ feature/account/src/main/java/com/bellogatecaliphate/account/ui/composables/ContentHistoryGridList.kt
@@ -2,14 +2,16 @@ package com.bellogatecaliphate.account.ui.composables
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -36,13 +37,15 @@ internal fun ContentHistoryGridList(
 	list: LazyPagingItems<Content>,
 	onOpenContent: (contentId: Content) -> Unit
 ) {
-	if (list.itemSnapshotList.isEmpty()) {
-		Box(contentAlignment = Alignment.Center) {
-			Text(stringResource(R.string.user_has_no_content))
-		}
-		return
-	}
-	LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)) {
+	LazyVerticalStaggeredGrid(
+		columns = StaggeredGridCells.Fixed(2),
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(8.dp),
+		contentPadding = PaddingValues(8.dp),
+		verticalItemSpacing = 8.dp,
+		horizontalArrangement = Arrangement.spacedBy(8.dp)
+	) {
 		items(list.itemCount) { index ->
 			val content = list[index]
 			content?.let {
