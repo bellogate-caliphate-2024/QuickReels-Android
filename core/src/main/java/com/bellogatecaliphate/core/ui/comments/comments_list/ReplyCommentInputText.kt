5c258054928a4e16600b877159fe788848782e10
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-07-26 01:00:16 +0100

    update

--- core/src/main/java/com/bellogatecaliphate/core/ui/comments/comments_list/ReplyCommentInputText.kt
+++ core/src/main/java/com/bellogatecaliphate/core/ui/comments/comments_list/ReplyCommentInputText.kt
@@ -7,18 +7,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bellogatecaliphate.core.R
 import com.bellogatecaliphate.core.util.PLACEHOLDER_16DP
 import com.bellogatecaliphate.core.util.PLACEHOLDER_8DP
@@ -33,10 +37,16 @@ internal fun ReplyCommentInputText(
 	var text by remember { mutableStateOf("") }
 	
 	Column(Modifier.fillMaxWidth()) {
-		OutlinedTextField(
-			modifier = Modifier.fillMaxWidth(),
+		TextField(
+			modifier = Modifier
+				.fillMaxWidth(),
 			value = text,
-			onValueChange = { text = it }
+			onValueChange = { text = it },
+			shape = RoundedCornerShape(8.dp),
+			colors = TextFieldDefaults.colors(
+				focusedIndicatorColor = Color.Transparent,
+				unfocusedIndicatorColor = Color.Transparent
+			)
 		)
 		Spacer(modifier = Modifier.height(PLACEHOLDER_8DP))
 		Row {
