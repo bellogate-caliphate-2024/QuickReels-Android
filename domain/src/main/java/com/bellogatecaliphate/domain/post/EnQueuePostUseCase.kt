50f4f0656d6b0c61139c38f843503dc3ae812a9f
Jeff Emuveyan <jeff.emuveyan@onefootball.com>
2025-09-30 14:57:58 +0100

    update

--- domain/src/main/java/com/bellogatecaliphate/domain/post/EnQueuePostUseCase.kt
+++ domain/src/main/java/com/bellogatecaliphate/domain/post/EnQueuePostUseCase.kt
@@ -1,23 +1,29 @@
 package com.bellogatecaliphate.domain.post
 
 import com.bellogatecaliphate.core.model.dto.Post
-import com.bellogatecaliphate.domain.user.GetUserInfoUseCase
 import com.bellogatecaliphate.post.IPostRepository
+import com.bellogatecaliphate.user.IUserRepository
 import javax.inject.Inject
 
 class EnQueuePostUseCase @Inject constructor(
-	private val getUserInfoUseCase: GetUserInfoUseCase,
+	private val userRepository: IUserRepository,
 	private val postRepository: IPostRepository
 ) {
 	
 	suspend operator fun invoke(post: Post) {
-		postRepository.uploadPost(
-			post.id,
-			post.videoFilePath,
-			getUserInfoUseCase()?.email ?: "",
-			post.time,
-			post.caption,
-			post.thumbnailFilePath ?: ""
-		)
+		try {
+			postRepository.uploadPost(
+				post.id,
+				post.videoFilePath,
+				userRepository.getUserEmail() ?: "",
+				post.time,
+				post.caption,
+				post.thumbnailFilePath ?: ""
+			)
+		}
+		catch (e: Exception) {
+			val x = e.message
+			val y = x?.length
+		}
 	}
 }
